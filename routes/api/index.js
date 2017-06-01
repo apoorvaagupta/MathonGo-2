5c0a175d51f3f677d58d9aa6c4319932a1a73db0
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-06-01 10:37:31 +0530

    Update index router

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,3 +1,8 @@
 const router = require('express').Router();
 
+router.use('/minicourses',require('./minicourses'));
+router.use('/lessons',require('./lessons'));
+router.use('/students',require('./students'));
+router.use('/tutors',require('./tutors'));
+
 module.exports = router;
