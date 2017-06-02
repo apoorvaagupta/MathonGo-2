9045123aa36912f7c099bc89dba30f0b90bf0658
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-06-02 21:07:40 +0530

    Update lessons router

--- routes/api/lessons.js
+++ routes/api/lessons.js
@@ -2,8 +2,17 @@ const router = require('express').Router();
 const models = require('./../../db/models').models;
 const password = require('./../../utils/password');
 
-router.get('/:lessonId', function (req, res) {
+router.get('/:id', function (req, res) {
     //get the lecture of this id
+    let lessonId = req.params.id;
+    models.Lesson.findOne({
+        where:{id:lessonId}
+    }).then(function (lesson) {
+        res.send(lesson);
+    }).catch(function (err) {
+        console.log(err);
+        res.send("Could not get this lesson right now");
+    })
 });
 
 router.post('/:lessonId/bookmark', function (req, res) {
@@ -14,7 +23,7 @@ router.post('/:lessonId/report', function (req, res) {
     //report this lesson
 });
 
-router.post('/:lessonId/upvote', function (req,res) {
+router.post('/:lessonId/upvote', function (req, res) {
     //upvote this lesson
 });
 
