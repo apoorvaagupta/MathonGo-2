bd671d8ccafa0f296d4b0171234ad4ebe3ffdf3c
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-06-05 23:08:24 +0530

    updated the passport dir

--- passport/passporthandler.js
+++ passport/passporthandler.js
@@ -1,3 +1,44 @@
-/**
- * Created by apoorvaa_gupta on 5/6/17.
- */
+const passport = require('passport');
+
+const UserStrategies = require('./strategies/user')
+    , ClientStrategies = require('./strategies/client')
+    , ApiStrategies = require('./strategies/api');
+
+const models = require('../db/models').models;
+
+const config = require('../config');
+
+passport.use(UserStrategies.localStrategy);
+passport.use(UserStrategies.fbStrategy);
+passport.use(UserStrategies.twitterStrategy);
+passport.use(UserStrategies.githubStrategy);
+passport.use(UserStrategies.lmsStrategy);
+
+passport.use(ClientStrategies.basicStrategy);
+passport.use(ClientStrategies.clientPasswordStrategy);
+
+passport.use(ApiStrategies.bearerStrategy);
+
+passport.serializeUser(function (user, cb) {
+    if (config.DEBUG) {
+        console.log("Serialize =  = = = ");
+        console.log(user);
+    }
+
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function (userid, cb) {
+    if (config.DEBUG) {
+        console.log("Deserialize =  = = = ");
+        console.log(userid);
+    }
+    models.User.findOne({
+        where: {id: userid}
+    }).then(function(user) {
+        return cb(null, user)
+    })
+});
+
+module.exports = passport;
+
