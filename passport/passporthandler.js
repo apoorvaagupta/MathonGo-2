960db60b74e1e986761ec5c5883936b64d0216ca
Apoorvaa Gupta <apoorvaagupta01@gmail.com>
2017-06-20 23:06:25 +0530

    updated the passport handler

--- passport/passporthandler.js
+++ passport/passporthandler.js
@@ -1,43 +1,19 @@
 const passport = require('passport');
 
-const localStudentStrategy = require('./strategies/local/student');
-const localTutorStrategy = require('./strategies/local/tutor');
-const localAdminStrategy = require('./strategies/local/admin');
-const bearerStudentStrategy = require('./strategies/bearer/student');
-const bearerTutorStrategy = require('./strategies/bearer/tutor');
-const bearerAdminStrategy = require('./strategies/bearer/admin');
+const localStrategy = require('./strategies/local/localstrategy');
+const bearerStrategy = require('./strategies/bearer/bearerstrategy');
 
 passport.serializeUser(function (user, cb) {
-    // if (config.DEBUG) {
-    //     console.log("Serialize =  = = = ");
-    //     console.log(user);
-    // }
-    //console.log(user);
     return cb(null, user);
 });
 //
 passport.deserializeUser(function (userid, cb) {
-    // if (config.DEBUG) {
-    //     console.log("Deserialize =  = = = ");
-    //     console.log(userid);
-    // }
-    // // models.User.findOne({
-    // //     where: {id: userid}
-    // // }).then(function(user) {
-    // //     return cb(null, user)
-    // // })
-
-    // console.log(userid);
     return cb(null, userid);
 });
 
 
-passport.use('local-student', localStudentStrategy);
-passport.use('local-tutor', localTutorStrategy);
-passport.use('local-admin', localAdminStrategy);
-passport.use('bearer-student', bearerStudentStrategy);
-passport.use('bearer-tutor', bearerTutorStrategy);
-passport.use('bearer-admin', bearerAdminStrategy);
+passport.use('local', localStrategy);
+passport.use('bearer', bearerStrategy);
 
 module.exports = passport;
 
