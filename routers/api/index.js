dee260dee40d7c0a4916905314166c61c06c717b
Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>
2017-07-16 21:54:57 +0530

    Update Reviews
    Signed-off-by: Bhavya Aggarwal <bhavyaaggarwal1209@gmail.com>

--- routers/api/index.js
+++ routers/api/index.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 const ensure = require('./../../passport/passportutils');
 const passport = require('./../../passport/passporthandler');
 
-router.use('/minicourses',require('./minicourses'));
-router.use('/lessons',  passport.authenticate('bearer'),require('./lessons'));
-router.use('/students',  passport.authenticate('bearer'),require('./students'));
+router.use('/minicourses', require('./minicourses'));
+router.use('/lessons', passport.authenticate('bearer'), require('./lessons'));
+router.use('/students', passport.authenticate('bearer'), require('./students'));
 //TODO : Add ensure admin
 router.use('/tutors', require('./tutors'));
 router.use('/extra', require('./extra'));
+router.use('/reviews', require('./reviews'));
 
 module.exports = router;
--- routers/api/reviews.js
+++ routers/api/reviews.js
@@ -24,13 +24,35 @@ router.get('/:miniCourseId', function (req, res) {
 router.post('/:miniCourseId', passport.authenticate('bearer'), ensure.ensureStudent(), function (req, res) {
   let miniCourseId = req.params.miniCourseId;
   models.Review.create({
-    rating: (+req.body.rating),
+    rating: (parseInt(req.body.rating)),
     description: req.body.description,
-    studentId: req.user.id,
     minicourseId: miniCourseId
   }).then(function (review) {
     if (review) {
       res.send({success: true, review: review})
+      models.MiniCourse.findOne({
+        where: {id: miniCourseId}
+      }).then(function (minicourse) {
+        let totalRating = minicourse.noOfRatings * minicourse.rating;
+        minicourse.noOfRatings++;
+        let finalRating = (totalRating + req.body.rating) / minicourse.noOfRatings;
+        if (req.body.description) {
+          minicourse.noOfReviews++;
+        }
+        minicourse.save();
+        models.MiniCourse.update({
+          rating: finalRating
+        }, {
+          where: {id: miniCourseId}
+        }).then(function () {
+
+        }).catch(function (err) {
+          console.log(err);
+        })
+
+      }).catch(function (err) {
+        console.log(err);
+      })
     } else {
       res.send({success: false})
     }
